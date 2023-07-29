6e0d95480efa6cf761f547683854828738c8128a
memchr <118117622+memchr@users.noreply.github.com>
2023-07-29 16:28:16 +0000

    feat: implement features/go

---
+++ .devcontainer/arch/features/go/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+user_name=${user_name:-$_CONTAINER_USER}
+packages=(
+	go
+	go-tools
+	gopls
+	delve
+	revive
+	staticcheck
+)
+
+##################
+# Install and configure the required packages.
+# globals:
+# 	packages[]
+##################
+install_packages() {
+	pacman -Syu --needed --ask 4 --noconfirm "${packages[@]}"
+	ln -sf /usr/bin/revive /usr/bin/golint
+}
+
+install_packages
