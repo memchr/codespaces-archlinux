1936f749f0815e6c5390000855e560c9ad77e71b
memchr <118117622+memchr@users.noreply.github.com>
2023-07-29 17:45:57 +0000

    fix(git): set pull.rebase to false

--- .devcontainer/arch/features/git/install.sh
+++ .devcontainer/arch/features/git/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-
+user_name=${user_name:-$_CONTAINER_USER}
 packages=(
 	git
 	onefetch
@@ -15,6 +15,10 @@ packages=(
 ##################
 install_packages() {
 	pacman -Syu --needed --ask 4 --noconfirm "${packages[@]}"
+}
+
+init_git() {
+	su -s /usr/bin/bash -c 'git config pull.rebase false' "${user_name}"
 }
 
 install_packages
