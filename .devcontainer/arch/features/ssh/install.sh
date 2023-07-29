0ea512722cb27d525682e65bef3b5a15d4054c39
memchr <118117622+memchr@users.noreply.github.com>
2023-07-29 16:59:53 +0000

    feat: implement features/ssh

---
+++ .devcontainer/arch/features/ssh/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+user_name=${user_name:-$_CONTAINER_USER}
+packages=(
+	openssh
+)
+
+##################
+# Install and configure the required packages.
+# globals:
+# 	packages[]
+##################
+install_packages() {
+	pacman -Syu --needed --ask 4 --noconfirm "${packages[@]}"
+}
+
+## generate random password for user
+init_passwd() {
+	_passwd="$(openssl rand -hex 16)"
+	cho "${user_name}:${_passwd}" | chpasswd
+}
+
+init_sshd() {
+	ssh-keygen -A
+	cp init-sshd.sh /usr/local/lib
+	chmod +x /usr/local/lib/init-sshd.sh
+}
+
+install_packages
+init_passwd
+init_sshd
