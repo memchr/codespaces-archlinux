fcc4a3c9f66c9cf82d972c9d87e25cfdb11e72be
memchr <118117622+memchr@users.noreply.github.com>
2023-07-29 16:18:49 +0000

    feat: implement features/python

---
+++ .devcontainer/arch/features/python/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+user_name=${user_name:-$_CONTAINER_USER}
+packages=(
+	python
+	python-pip
+	python-pipx
+	ipython
+	ruff
+	memray
+	yapf
+	python-black
+	autopep8
+)
+
+##################
+# Install and configure the required packages.
+# globals:
+# 	packages[]
+##################
+install_packages() {
+	pacman -Syu --needed --ask 4 --noconfirm "${packages[@]}"
+}
+
+install_packages
