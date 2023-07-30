449a7f9bd3fe0d429c2f07b3c2f37519a717ca18
memchr <118117622+memchr@users.noreply.github.com>
2023-07-30 08:15:36 +0000

    refactor: move test config to a separate directory

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
-docker run -it $(devcontainer build --workspace-folder . | jq  -r ".imageName[0]") bash 
+_test_release_version=0
+if [[ $1 == -r ]]; then
+	_test_release_version=1
+fi
+
+if ((_test_release_version)); then
+	docker run -it $(devcontainer build --workspace-folder . | jq  -r ".imageName[0]") bash 
+else
+	docker run -it $(devcontainer build --config .devcontainer/testing/devcontainer.json --workspace-folder . | jq  -r ".imageName[0]") bash 
+fi
 docker stop $(docker ps -a -q) > /dev/null
