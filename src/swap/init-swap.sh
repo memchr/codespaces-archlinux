85b61b6415120a7c01f64eaa99e770bfaad479d2
memchr <118117622+memchr@users.noreply.github.com>
2023-08-01 07:59:26 +0000

    feat: add swap creation

---
+++ src/swap/init-swap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2164
+ 
+if [[ $(id -u) != 0 ]]; then
+	srcpath="$(realpath "$0")"
+	exec sudo bash "$srcpath" "$@"
+fi
+
+cat > /tmp/mkswap.sh << 'EOF'
+dd if=/dev/zero bs=1M of=/tmp/swapfile count=2K
+chmod 644 /tmp/swapfile
+mkswap /tmp/swapfile
+swapon /tmp/swapfile
+EOF
+
+bash /tmp/mkswap.sh & disown
+
+exec "$@"
